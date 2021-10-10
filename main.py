a2160352bddf61f657c8a4c85bd45e011560a4d2
Myles Smith <4060003+xsd08tfu@users.noreply.github.com>
2021-10-10 12:00:19 +0100

    2 motors

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from gpiozero import PWMOutputDevice
 from time import sleep, time
 
-motor = PWMOutputDevice(21)
-motor.value = 1.0
+motor1 = PWMOutputDevice(21)
+motor2 = PWMOutputDevice(20)
+motor1.value = 1.0
+motor2.value = 1.0
 
-sleep(2)
-motor.value = 0.5
+sleep(5)
+motor1.value = 0.75
+motor2.value = 0.75
 
-sleep(2)
-motor.value = 0.1
-
+sleep(5)
+motor1.value = 0.5
+motor2.value = 0.5
+sleep(5)
 exit()
