b72851c687a0a5d97f47b71ddb4a96d3c19a75a9
Myles Smith <4060003+xsd08tfu@users.noreply.github.com>
2021-10-10 14:25:12 +0100

    update

--- main_debug.py
+++ main_debug.py
@@ -5,9 +5,14 @@ motor1 = PWMOutputDevice(21)
 motor2 = PWMOutputDevice(20)
 
 def MotorControl(power,time):
+    print("Setting motors to "+str(power*100)+"%")
     motor1.value = power
     motor2.value = power
-    sleep(time)
+    sleep(time-10)
+    print("Changing in 10")
+    sleep(5)
+    print("Changing in 5")
+    sleep(5)
 
 while True:
     MotorControl(1,30)
