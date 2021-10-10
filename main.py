31263909a9b5877a9bd96b557628293bb3ff0d5f
Myles Smith <4060003+xsd08tfu@users.noreply.github.com>
2021-10-10 12:14:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,16 @@ motor2 = PWMOutputDevice(20)
 print("Running at 100%")
 
 while True:
+    print("Both at 100%")
     motor1.value = 1
     motor2.value = 1
+    sleep(100)
+    print("Both at 90%")
+    motor1.value = 0.9
+    motor2.value = 0.9
+    sleep(100)
+    print("Both at 0%")
+    motor1.value = 0
+    motor2.value = 0
+    sleep(10)
+    
