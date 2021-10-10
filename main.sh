fcff8a3c8b86073fcebb402b917e5119c4a62532
Myles Smith <4060003+xsd08tfu@users.noreply.github.com>
2021-10-10 15:19:55 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,8 +8,9 @@ ping -c 2 www.google.com
 if [ $? != 0 ] 
 then
    echo "does not have internet"
-   python3 main.py
+   python3 /home/pi/code/haptic-motors/main.py
 else
    echo "has internet"
+   cd /home/pi/code/haptic-motors/
    git pull && python3 main.py
 fi
