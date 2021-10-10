7b86d78dd13d3b643800c8446e9be2b30d0c3527
Myles Smith <4060003+xsd08tfu@users.noreply.github.com>
2021-10-10 12:19:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,19 @@ while True:
     print("Both at 100%")
     motor1.value = 1
     motor2.value = 1
-    sleep(100)
+    sleep(50)
+    print("Changing in 10")
+    sleep(10)
     print("Both at 90%")
     motor1.value = 0.9
     motor2.value = 0.9
-    sleep(100)
+    sleep(50)
+    print("Changing in 10")
+    sleep(10)
     print("Both at 0%")
     motor1.value = 0
     motor2.value = 0
+    sleep(50)
+    print("Changing in 10")
     sleep(10)
     
