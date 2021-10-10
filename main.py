50ab3e054901277b531c10d2ed4f4cefc79a2e24
Myles Smith <4060003+xsd08tfu@users.noreply.github.com>
2021-10-10 11:50:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from gpiozero import PWMOutputDevice
 from time import sleep, time
 
 motor = PWMOutputDevice(21)
+motor.value = 1.0
+
+sleep(2)
 motor.value = 0.5
 
 sleep(2)
+motor.value = 0.1
+
 exit()
