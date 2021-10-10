4a02c1f328b2107e1f53c38011ed749b12fff437
Myles Smith <4060003+xsd08tfu@users.noreply.github.com>
2021-10-10 12:02:01 +0100

    add commentary

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ from time import sleep, time
 
 motor1 = PWMOutputDevice(21)
 motor2 = PWMOutputDevice(20)
-motor1.value = 1.0
-motor2.value = 1.0
+print("Running at 50%")
+motor1.value = 0.5
+motor2.value = 0.5
 
 sleep(5)
+print("Running at 75%")
 motor1.value = 0.75
 motor2.value = 0.75
 
 sleep(5)
-motor1.value = 0.5
-motor2.value = 0.5
+
+print("Running at 100%")
+motor1.value = 1
+motor2.value = 1
 sleep(5)
+print("Ending")
 exit()
